b4852d05d148bb4cdd22f24309bfec407cf62601
aktasbatuhan <aktasbatuhann@gmail.com>
2024-04-23 17:57:13 +0300

    hot fix

--- db/mongodb/interface.py
+++ db/mongodb/interface.py
@@ -1,10 +1,10 @@
-import os
 from db.mongodb import MongoClient, MongoReader, ReaderWriter, MongoWriter
+import streamlit as st
 
 
 class MongoEngine:
     def __init__(self):
-        self.__mongo_uri = os.environ["mongo"]["MONGODB_URI"]
+        self.__mongo_uri = st.secrets["MONGODB_URI"]
         self.__user_client_reader = MongoClient(self.__mongo_uri, "reader")
         self.__user_client_writer = MongoClient(self.__mongo_uri, "writer")
 
